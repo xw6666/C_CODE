64fe5e07bebc9d954e7c2b5c91edc8935799ee1c
xw6666 <1481016131@qq.com>
2021-06-03 22:02:17 +0800

    6月3日小练习

---
+++ test_6_2/test_6_2.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main(void)
+//{
+//	//库函数 - 打印函数 - 输出函数
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//	printf("hello world!\n");
+//	printf("Hello world!\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 10;
+//	int price = 50;
+//	double weight = 55.5;//双精度浮点型 - 为什么叫浮点型 - 因为小数点可以浮动
+//
+//	return 0;
+//
+
+//int main()
+//{
+//	//如何计算一个类型创建的变量空间的大小
+//	//sizeof()
+//	//%d - 整型
+//	//%c - 打印字符
+//	//printf("%d\n", 100);
+//	//printf("%c\n", 100);
+//	//printf("%d\n", sizeof(char));
+//	//printf("%d\n", sizeof(short));
+//	//printf("%d\n", sizeof(int));
+//	//printf("%d\n", sizeof(long));
+//	//printf("%d\n", sizeof(long long));
+//	//printf("%d\n", sizeof(float));
+//	//printf("%d\n", sizeof(double));
+//	return 0;
+//
+
+//int main()
+//{
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x;
+//	scanf("%d", &x);
+//	printf("x = %d\n", x);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x;
+//	scanf("%d", &x);
+//	printf("x = %d\n", x);
+//	return 0;
+//}
