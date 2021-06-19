4605fa1c576696684ff70934914130f422d6d7b1
xw6666 <77912059+xw6666@users.noreply.github.com>
2021-06-19 23:26:09 +0800

    leetcode练习

--- 2021_6_19/test.c
+++ 2021_6_19/test.c
@@ -268,3 +268,27 @@
 //
 //    return dp[len - 1];
 //}
+
+//leetcode - 198打家劫舍
+int rob(int* nums, int numsSize) {
+    if (numsSize == 1)
+    {
+        return nums[0];
+    }
+    if (numsSize == 2)
+    {
+        if (nums[0] > nums[1])
+        {
+            return nums[0];
+        }
+        return nums[1];
+    }
+    int dp[numsSize];  //dp数组表示0到i长度的房屋能偷窃最大金额为dp[i]
+    dp[0] = nums[0];
+    dp[1] = fmax(nums[0], nums[1]);
+    for (int i = 2; i < numsSize; i++)
+    {
+        dp[i] = fmax(dp[i - 2] + nums[i], dp[i - 1]);
+    }
+    return dp[numsSize - 1];
+}
