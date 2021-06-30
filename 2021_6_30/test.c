c280945e4528dd2956e28b13256bd557893d8336
xw6666 <77912059+xw6666@users.noreply.github.com>
2021-06-30 21:28:44 +0800

    更新了同学期末考问的问题

--- 2021_6_30/test.c
+++ 2021_6_30/test.c
@@ -75,3 +75,173 @@
 //	printf("a = %d, b = %d\n", a, b);
 //	return 0;
 //}
+
+//leetcode - 8. 字符串转换整数(atoi)
+//int myAtoi(char* s) {
+//    long long ret = 0;
+//    while (*s == ' ')
+//    {
+//        s++;
+//    }
+//    if (*s == '-')
+//    {
+//        s++;
+//        if ((*s >= '0' && *s <= '9'))
+//        {
+//            while ((*s >= '0' && *s <= '9'))
+//            {
+//                ret = ret * 10 - (*s - '0');
+//                s++;
+//                if (ret < INT_MIN)
+//                {
+//                    return INT_MIN;
+//                }
+//            }
+//            return ret;
+//        }
+//        else
+//        {
+//            return 0;
+//        }
+//    }
+//    else
+//    {
+//        if ((*s >= 'A' && *s <= 'Z') || (*s >= 'a' && *s <= 'z'))
+//        {
+//            //第一个是字母
+//            return 0;
+//        }
+//        else if (*s == '+')
+//        {
+//            //第一个是加号
+//            s++;
+//            if ((*s >= '0' && *s <= '9'))
+//            {
+//                while ((*s >= '0' && *s <= '9'))
+//                {
+//                    ret = ret * 10 + (*s - '0');
+//                    s++;
+//                    if (ret > INT_MAX)
+//                    {
+//                        return INT_MAX;
+//                    }
+//                }
+//                return ret;
+//            }
+//            else
+//            {
+//                return 0;
+//            }
+//
+//        }
+//        else
+//        {
+//            //第一个是数字
+//            while ((*s >= '0' && *s <= '9'))
+//            {
+//                ret = ret * 10 + (*s - '0');
+//                s++;
+//                if (ret > INT_MAX)
+//                {
+//                    return INT_MAX;
+//                }
+//            }
+//            return ret;
+//        }
+//    }
+//    return 0;
+//}
+
+
+//去重
+//int main()
+//{
+//	char arr[8] = { 'a','a','a','b','b','v','c','c' };//8个元素
+//	int num = 8;
+//	int begin = 0;
+//	while (begin < num - 1)
+//	{
+//		int search = begin + 1;
+//		if (arr[begin] == arr[search])
+//		{
+//			//相等覆盖
+//			while (search < num - 1)
+//			{
+//				arr[search] = arr[search + 1];
+//				search++;
+//			}
+//			num--;
+//		}
+//		else
+//		{
+//			//不相等移位
+//			begin++;
+//		}
+//	}
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		printf("%c", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//同学问的问题
+//struct student
+//{
+//	int order;  //学号
+//	char name[30];//姓名
+//	//成绩
+//	float a;
+//	float b;
+//	float c;
+//};
+//int main(int argc, char* argv[]) {
+//	struct student stu[3] = { 0 };
+//	int i = 0;
+//	//t代表成绩，需要用浮点型
+//	float t = 0;
+//	//输入信息
+//	for (i = 0; i < 3; i++)
+//	{
+//		scanf("%d %s %f %f %f", &(stu[i].order), &(stu[i].name), &(stu[i].a), &(stu[i].b), &(stu[i].c));
+//	}
+//	for (i = 0; i < 3; i++)
+//	{
+//		//如果a<b ,t = b,else t = a
+//		if (stu[i].a < stu[i].b)
+//		{
+//			//t = stu[i].a;
+//			//只需要找到最大的就行了
+//			//stu[i].a = stu[i].b;
+//			//stu[i].b = t;
+//			t = stu[i].b;
+//		}
+//		else
+//		{
+//			t = stu[i].a;
+//		}
+//		//接下来用t对比，找出三者的最大值
+//		if (t < stu[i].c)
+//		{
+//			t = stu[i].c;
+//			//stu[i].a = stu[i].c;
+//			//stu[i].c = t;
+//		}
+//		//执行到这里就找到了第i组的最好成绩了，为t
+//		printf("%d %s %f\n", stu[i].order, stu[i].name, t);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[20] = { 0 };
+//	int num = 0;
+//	while (scanf("%c", &arr[num]), arr[num] != '@')
+//	{
+//		num++;
+//	}
+//	printf("num = %d\n", num);
+//	return 0;
+//}
