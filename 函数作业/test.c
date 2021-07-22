598176b5be9adc9b0784181c8a62dfa63aedad86
晓伍 <1481016131@qq.com>
2021-07-22 17:35:36 +0800

    增加了c语言函数作业

---
+++ 函数作业/test.c
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+//bool isPrime(int x)
+//{
+//	if (x < 2)
+//	{
+//		return false;
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 2; i <= sqrt(x); i++)
+//		{
+//			if (x % i == 0)
+//			{
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (isPrime(i))
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//bool isLeapYear(int x)
+//{
+//	if ((x % 4 == 0 && x % 100 != 0) || (x % 400 == 0))
+//	{
+//		return true;
+//	}
+//	else
+//	{
+//		return false;
+//	}
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1000; i <= 2000; i++)
+//	{
+//		if (isLeapYear(i))
+//		{
+//			printf("%d是闰年\n", i);
+//		}
+//		else
+//		{
+//			printf("%d不是闰年\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//void exange(int* a, int* b)
+//{
+//	int temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//
+//int main()
+//{
+//	int a = 5;
+//	int b = -6;
+//	printf("交换前：a = %d, b = %d\n", a, b);
+//	exange(&a, &b);
+//	printf("交换后：a = %d, b = %d\n", a, b);
+//}
+
+void Print(int x)
+{
+	int i = 0;
+	for (i = 1; i <= x; i++)
+	{
+		int j = 0;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%2d=%-4d", j, i, j * i);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	Print(n);
+	return 0;
+}
