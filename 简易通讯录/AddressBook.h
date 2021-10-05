0ccbb63abe96b14dd8600d9ac88a47b69ea8f1db
xw6666 <1481016131@qq.com>
2021-10-05 23:17:00 +0800

    更新简易版c语言通讯录

--- 简易通讯录/AddressBook.h
+++ 简易通讯录/AddressBook.h
@@ -1,11 +1,38 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
+#include <assert.h>
+#define MAXSIZE 1000
+#define NAMEMAX 20
+#define TELEMAX 20
+#define SEXMAX 5
+#define ADDMAX 30
 
-typedef struct data {
-	char name[20];
-	char sex[10];
+enum Choice
+{
+	EXIT,
+	ADD,
+	DEL,
+	MODIFY,
+	SEARCH,
+	SHOW,
+	SORT
+};
+
+typedef struct Person {
+	char name[NAMEMAX];
+	char sex[SEXMAX];
 	int age;
-	char tele[12];
-	char address[50];
-}data;
+	char tele[TELEMAX];
+	char address[ADDMAX];
+}Person;
 
+//通讯录
+typedef struct Contact {
+	int size;  //记录当前已存入人数
+	Person data[MAXSIZE];
+}Contact;
+
+//增加联系人
+void add(Contact* contact);
